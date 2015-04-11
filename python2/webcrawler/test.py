1b54fa283853c5e9f739f213adacdadc52c2481c
admin <daitaomail@gmail.com>
2015-04-11 04:13:58 -0700

    test

--- python2/webcrawler/test.py
+++ python2/webcrawler/test.py
@@ -17,12 +17,15 @@ class ThreadTest(threading.Thread):
 
 
 if __name__ == '__main__':
-    os.system('printf "\033c"')
+	#os.system('printf "\033c"')
+	tmp_list = []
+	for i in xrange(10000000):
+		tmp_list.append(i)
 
-    for i in xrange(5):
-        thread = ThreadTest()
-        thread.start()
-
-    print thread_queue.qsize()
-
-
+	tmp_len = len(tmp_list)
+	#for i in xrange(tmp_len):
+	for i in xrange(len(tmp_list)):
+		tmp_list[i] = 'test'
+		pass
+	
+	print len(tmp_list)
