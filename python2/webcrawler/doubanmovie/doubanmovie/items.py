9e2ec004e1d6e1c50380ae8a0ac6b7f5110f3cb6
admin <daitaomail@gmail.com>
2015-04-12 07:07:37 -0700

    add scrapy project

---
+++ python2/webcrawler/doubanmovie/doubanmovie/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+#import scrapy
+from scrapy import Field, Item
+
+
+class DoubanmovieItem(Item):
+	movie_name = Field()
+	movie_director = Field()
+	movie_starts = Field()
+	movie_rating = Field()
+	movie_quote = Field
---
+++ python2/webcrawler/doubanmovie/doubanmovie/pipelines.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import codecs
+
+class DoubanmoviePipeline(object):
+	def __init__(self):
+		self.file = codecs.open('doubanmovive_data.json', mode='w', encoding='utf-8')
+	
+	def process_item(self, item, spider):
+		line = json.dumps(dict(item)) + '\n'
+		self.file.write(line)
+		return item
