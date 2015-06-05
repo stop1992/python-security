045b146f6cc2ab65ad00b0d5c5b770a74c3d5f2a
daitao <daitaomail@gmail.com>
2015-06-05 15:38:04 +0800

    change files

--- webcrawler/mongosharding.py
+++ webcrawler/mongosharding.py
@@ -38,6 +38,7 @@ def start_localmongo():
 
 def prepare(child):
 	child.sendline('rm -rf /data')
+	child.sendline('rm -rf /data')
 	child.sendline('kill 2 `pgrep mongo`')
 	child.sendline('kill 2 `pgrep mongo`')
 
@@ -59,6 +60,7 @@ def start_dtpc2(child):
 	child.sendline('mongod --shardsvr --replSet shard1 --dbpath /data/shard1_2/db --logpath /data/shard1_2/log/server.log --fork --port 27018 --directoryperdb --keyFile /data/shard1_2/key')
 	# mongod --replSet shard1 --port 27020 --dbpath /data/shard1_1/arb --keyFile /data/shard1_1/key --fork --logpath /data/shard1_1/arbiter.log
 	child.sendline('mongod --shardsvr --replSet shard2 --dbpath /data/shard2_2/db --logpath /data/shard2_2/log/server.log --fork --port 27019 --directoryperdb --keyFile /data/shard2_2/key')
+	# child.sendline('mongod --shardsvr --replSet shard2 --dbpath /data/shard2_2/db --logpath /data/shard2_2/log/server.log --fork --port 27019 --directoryperdb --keyFile /data/shard2_2/key')
 	# mongod --replSet shard2 --dbpath /data/shard2_1/arb --port 27021 --keyFile /data/shard2_1/key --fork --logpath /data/shard2_1/arbiter.log
 
 	# prepare configserver files
@@ -90,6 +92,8 @@ def start_dtpc3(child):
 	child.sendline('mongod --shardsvr --replSet shard1 --dbpath /data/shard1_3/db --logpath /data/shard1_3/log/server.log --fork --port 27018 --directoryperdb --keyFile /data/shard1_3/key')
 	# mongod --replSet shard1 --port 27020 --dbpath /data/shard1_1/arb --keyFile /data/shard1_1/key --fork --logpath /data/shard1_1/arbiter.log
 	child.sendline('mongod --shardsvr --replSet shard2 --dbpath /data/shard2_3/db --logpath /data/shard2_3/log/server.log --fork --port 27019 --directoryperdb --keyFile /data/shard2_3/key')
+	# child.sendline('mongod --shardsvr --replSet shard2 --dbpath /data/shard2_3/db --logpath /data/shard2_3/log/server.log --fork --port 27019 --directoryperdb --keyFile /data/shard2_3/key')
+	# child.sendline('mongod --shardsvr --replSet shard2 --dbpath /data/shard2_3/db --logpath /data/shard2_3/log/server.log --fork --port 27019 --directoryperdb --keyFile /data/shard2_3/key')
 	# mongod --replSet shard2 --dbpath /data/shard2_1/arb --port 27021 --keyFile /data/shard2_1/key --fork --logpath /data/shard2_1/arbiter.log
 
 	# prepare configserver files
@@ -128,16 +132,25 @@ if __name__ == '__main__':
 	os.system('printf "\033c"')
 
 	start_localmongo()
+	print '\n\n\n\n'
 
 	users = 'root'
 	hosts = []
 	hosts.append('192.168.1.110')
 	hosts.append('192.168.1.112')
+	print 'start 192.168.1.110'
 	child_2 = ssh_command(users, hosts[0])
 	start_dtpc2(child_2)
+	print '\n\n\n\n'
+
+	print 'start 192.168.1.112'
 	child_3 = ssh_command(users, hosts[1])
 	start_dtpc3(child_3)
+	print '\n\n\n\n'
 
+	print 'start localmongos\n\n\n'
 	start_localmongos()
+	print 'start mongos2\n\n\n'
 	start_mongos(child_2)
-	start_mongos(child_3)
+	print 'start mongos3\n\n\n'
+	start_mongos(child)
--- webcrawler/test.sh
+++
@@ -1,1 +0,0 @@
-kill -2 `pgrep mongo`
