d7d0f9ae7fb6af4324232b68b0a999789360b8ed
daitao <daitaomail@gmail.com>
2015-06-06 16:22:51 +0800

    add files

--- webcrawler/test/guba/guba/settings.py
+++ webcrawler/test/guba/guba/settings.py
@@ -16,5 +16,12 @@ NEWSPIDER_MODULE = 'guba.spiders'
 ITEM_PIPELINES = {
 	'guba.pipelines.GubaPipeline':300
 	}
+
+SCHEDULER = 'guba.scrapy_redis.scheduler.Scheduler'
+SCHEDULER_PERSIST = True
+SCHEDULER_QUEUE_CLASS = 'guba.scrapy_redis.queue.SpiderPriorityQueue'
+
+REDIS_HOST = '192.168.1.108'
+REDIS_PORT = 6379
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'guba (+http://www.yourdomain.com)'
