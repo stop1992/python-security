f0c0b030358806d567aa83975f6268dca53cfbe2
xinali <daitaomail@gmail.com>
2015-06-09 22:26:38 +0800

    test

--- webcrawler/test.py
+++ webcrawler/test.py
@@ -1,14 +1,21 @@
+#-*- encoding:utf-8 -*-
+
 import xlrd
 import os
 
 if __name__ == '__main__':
 	os.system('printf "\033c"')
 
-	data = xlrd.open_workbook('data.xls')
-	table = data.sheets()[0]
-	colums = table.col_values(1)
+	# data = xlrd.open_workbook('data.xls')
+	# table = data.sheets()[0]
+	# colums = table.col_values(1)
 
-	for item in colums:
-		if item:
-			print item
-			raw_input('press')
+	a = ['test', 'wangxi', 123, 'daitao']
+	b = 'test wangxi haha hello world daitao'
+	for item in a:
+		if b.find(str(item)) > 0:
+			print item, 'in b'
+	# for item in colums:
+		# if item:
+			# print item
+			# raw_input('press')
--- webcrawler/test/guba/guba/spiders/spider.py
+++ webcrawler/test/guba/guba/spiders/spider.py
@@ -1,7 +1,6 @@
 #-*- encoding:utf-8 -*-
 
 from scrapy.spider import BaseSpider
-# from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
 import re
 
