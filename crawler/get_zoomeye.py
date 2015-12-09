c9c6ae0b5d84a8b68252e2536a0f83cb2444fe3f
daitao <daitaomail@gmail.com>
2015-12-09 22:54:57 +0800

    change files

--- crawler/get_zoomeye.py
+++ crawler/get_zoomeye.py
@@ -9,6 +9,12 @@ import urllib2
 import cookielib
 from selenium import webdriver
 import time
+import pdb
+
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 class ZoomEye(object):
     def __init__(self):
@@ -30,61 +36,69 @@ class ZoomEye(object):
                 'DNT':1,
                 'Connection':Connection
                 }
-        # self.url = 'https://www.zoomeye.org/search?q=app%3A%22discuz%22%20php%3A%225.5.7%22&p=6&t=web'
+        self.fp = open('relative_sites.txt', 'w')
+        self.fp.close()
 
-
-    def get_first_page(self):
+    def get_site(self):
 
         service_args = [
             '--proxy=139.196.108.68:80'
             '--proxy-type=https'
             ]
 
-        driver = webdriver.PhantomJS(service_args=service_args)
+        self.driver = webdriver.PhantomJS(service_args=service_args)
 
         # fist step, get cookie by request url
         url = 'https://www.zoomeye.org'
-        driver.get(url)
-        driver.refresh()
-        # time.sleep(2)
-        return driver
+        self.driver.get(url)
+        self.driver.refresh()
+
+    def write_results2file(self):
+
+        ul_element = self.driver.find_elements_by_css_selector('.result.web')[0]
+        a_elements = ul_element.find_elements_by_css_selector('h3>a')
+
+        self.fp = open('relative_sites.txt', 'a+')
+
+        for a in a_elements:
+            href = a.get_attribute('href')
+            print 'get ' + href
+            self.fp.write(href + '\n')
+
+        self.fp.close()
+
 
     def get_result(self):
 
-        driver = get_first_page()
+        self.get_site()
 
         print '-' * 80
+        print 'getting page 1....'
         search_dork = 'https://www.zoomeye.org/search?q=app%3Adiscuz%20php%3A%225.5.7%22&p=1&t=web'
-        driver.get(search_dork)
+        self.driver.get(search_dork)
+        self.write_results2file()
 
+        result_summary = self.driver.find_elements_by_class_name('result-summary')[0]
+        result_counts = int(result_summary.text.split()[2])
+        if result_counts%10:
+            pages = result_counts / 10 + 1
+        else:
+            pages = result_counts / 10
+
+        for page in xrange(2, pages+1):
+            print '-' * 70
+            print 'getting page ', page, '.....'
+            search_dork = 'https://www.zoomeye.org/search?q=app%3Adiscuz%20php%3A%225.5.7%22&p=' + str(page) + '&t=web'
+            self.driver.get(search_dork)
+            self.write_results2file()
+            time.sleep(3)
 
         driver.quit()
 
 
-def test_req():
-
-    url = 'http://www.zhihu.com'
-    req1 = urllib2.Request(url)
-    response = urllib2.urlopen(req1)
-    cookie = response.headers.get('Set-Cookie')
-    print response.getcode()
-
-    url = 'http://www.zhihu.com'
-    req2 = urllib2.Request(url)
-    req2.add_header('cookie', cookie)
-    response = urllib2.urlopen(req2)
-    print response.getcode()
-
-    # url = 'https://www.zoomeye.org'
-    # response = urllib2.urlopen(url).read()
-    # print response
-
 def main():
     zoom = ZoomEye()
-    # zoom.get_html()
-    # zoom.get_html_req()
-    zoom.get_html_driver()
-    # test_req()
+    zoom.get_result()
 
 if __name__ == '__main__':
     os.system('clear')
--- crawler/test.py
+++ crawler/test.py
@@ -32,53 +32,20 @@ def test15():
 
     print lack_queue.qsize()
 
+def tt():
+    fp = open('result.txt', 'a+')
+    for i in xrange(10):
+        fp.write('this is a test' + '\n')
+    fp.write('\n')
+    fp.close()
 
-def test14():
-    server = MongoClient('localhost', 27018)
-    db = server.guba
-    lack_queue = Queue.Queue()
-    line_num = 1
-    for stock_num in open('stocknums.txt', 'r'):
-        stocknum = 'db' + stock_num.strip()
-        collec = db[stocknum]
-        # print collec.count()
-        if collec.count() == 0:
-            print stock_num.strip(), 'is not exist'
-            lack_queue.put(stock_num)
-            # print line_num
-        line_num += 1
-
-    print lack_queue.qsize()
-
-
-def test16():
-    host = '192.168.1.108'
-    port = 27018
-    client = MongoClient(host, port)
-    db = client.guba
-
-    col = db['db000002']
-    a = col.find()
-    pdb.set_trace()
-    for i in a:
-        # pdb.set_trace()
-        print type(i)
-        # raw_input('please')
-
-
-def add_small():
-    tmp_txt = open('result.txt', 'r').readlines()
-    fp = open('tmp.txt', 'w')
-    start = 0
-    for i in xrange(0, len(tmp_txt)):
-        if i % 2 == 0:
-            fp.write('>' + tmp_txt[i])
-        else:
-            fp.write(tmp_txt[i])
+def read_file():
+    fp = open('result.txt', 'r')
+    print fp.readlines()
     fp.close()
 
 if __name__ == '__main__':
     os.system('printf "\033c"')
 
-    add_small()
-
+    tt()
+    read_file()
