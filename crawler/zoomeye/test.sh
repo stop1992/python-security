5a2102408d8284d2a2f41cc523e37db6feb41700
xinali <daitaomail@gmail.com>
2016-06-16 19:46:40 +0800

    change some files

--- crawler/zoomeye/test.sh
+++
@@ -1,2 +0,0 @@
-# curl -X POST http://api.zoomeye.org/user/login -d '{ "username": "testmai1@sina.com", "password": "testTEST"}'
-curl -X POST http://api.zoomeye.org/user/login -d '{ "username": "daitaomail@gmail.com", "password": "zoomeyeZOOMEYE68"}'
--- crawler/zoomeye/text.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST http://api.zoomeye.org/user/login -d
-'{
-    "username": "testmai1@sina.com",
-    "password": "testTEST"
-}'
