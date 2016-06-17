5e0633f4fef4f9409dec07d829c1b09e41807faa
xinali <daitaomail@gmail.com>
2016-06-17 15:00:08 +0800

    add dirbrute dir

---
+++ security/utils/dirbrute/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+sys.path.append('../')
+import os
+from pocsuite.thirdparty.termcolor.termcolor import cprint
+
+
+if __name__ == '__main__':
+    os.system('clear')
+
+    cprint('test', 'red')
+    cprint('test', 'white')
+
+
