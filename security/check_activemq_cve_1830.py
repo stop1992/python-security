e09c8764091f7f5549fe10e2312ea7722ce55eb9
xinali <daitaomail@gmail.com>
2016-06-26 21:58:09 +0800

    change check activemq

--- security/check_activemq_cve_1830.py
+++ security/check_activemq_cve_1830.py
@@ -7,9 +7,9 @@ import os
 import threading
 import time
 from Queue import Queue
+import netaddr
 
 from utils.randomuseragent import randomuseragent
-
 from pocsuite.lib.core.data import logger
 from pocsuite.lib.core.enums import CUSTOM_LOGGING
 from pocsuite.lib.core.exception import PocsuiteThreadException
@@ -18,33 +18,63 @@ from pocsuite.lib.core.exception import PocsuiteThreadException
 class Activemq(object):
 
     def __init__(self):
-        pass
 
-    def check_cve_2016_3088(self, url):
+        self.in_queue = Queue()
+        # for site in open('top_sites.txt', 'r'):
+            # self.in_queue.put('http://www.' + site.strip() + ':8161')
+        for ip_CIDR in open('china_ip_list.txt', 'r'):
+            cidr = int(ip_CIDR.split('/')[1])
+            if cidr < 24:
+                continue
+            ips = netaddr.IPNetwork(ip_CIDR.strip())
+            # print len(ips)
+            for ip in ips:
+                # self.in_queue.put('http://' + str(ip).strip() + ':8161')
+                self.in_queue.put('http://' + str(ip).strip() + ':80')
+
+        # print 'totalsite:', self.in_queue.qsize()
+        self.total_sites = self.in_queue.qsize()
+        self.success_queue = Queue()
+
+
+    def check_cve_2016_3088(self):
         '''
         affected version:
         Apache ActiveMQ 5.0.0 - 5.13.2
         '''
 
-        requests = requests.Session()
-        url = 'http://192.168.1.101:8161'
-        headers = {
-                "Authorization": "Basic YWRtaW46YWRtaW4=",
-                "User-Agent": randomuseragent('useragents.txt')
-                }
-        response = requests.get(url, headers=headers, timeout=5)
-        # print response.text
-        print response.status_code
+        # requests = requests.Session()
+        while self.in_queue.qsize() > 0:
+            url = self.in_queue.get()
+            headers = {
+                    "Authorization": "Basic YWRtaW46YWRtaW4=",
+                    "User-Agent": randomuseragent('useragents.txt')
+                    }
+            try:
+                # print url
+                response = requests.get(url, headers=headers, timeout=5)
+                # print response.text
+                # print url
+                # print url, response.status_code
+                if response.status_code == '200':
+                    print url, response.status_code, 'successfully...'
+                    self.success_queue.put(url)
+            except Exception, e:
+                pass
+                # print e
+            sys.stdout.write('\r' + ' ' * 80 + str(self.in_queue.qsize()) + ' remain ' + \
+                                'total: ' + str(self.total_sites))
+            sys.stdout.flush()
 
 
     def run(self):
 
         threads = []
-        thread_nums = 20
+        thread_nums = 60
 
         try:
             for thread_num in xrange(thread_nums):
-                thread = threading.Thread(target=check_cve_2016_3088, name=str(thread_num))
+                thread = threading.Thread(target=self.check_cve_2016_3088, name=str(thread_num))
                 thread.setDaemon(True)
                 thread.start()
                 threads.append(thread)
@@ -73,8 +103,11 @@ class Activemq(object):
 def main():
 
     activemq = Activemq()
-    activemq.check_cve_2016_3088('ll')
+    activemq.run()
 
+    print '=' * 20 + ' successfully ' + 20 * '='
+    while activemq.success_queue.qsize() > 0:
+        print activemq.get()
 
 
 if __name__ == '__main__':
--- security/get_top500.py
+++ security/get_top500.py
@@ -8,8 +8,6 @@ import re
 import time
 import subprocess
 
-from get_curl_header_post import get_curl_header_post
-
 class Sites(object):
 
     def __init__(self):
@@ -29,27 +27,6 @@ class Sites(object):
         self.sites_file.close()
 
 
-    def get_txt_record(self):
-
-        record_fp = open('txt_recorder.txt', 'w')
-        total_no_record = 0
-
-
-        # for s in open('top.txt', 'r'):
-        for s in open('top_sites.txt', 'r'):
-            host_sub = subprocess.Popen(['host', '-t', 'txt', s.strip()], stdout=subprocess.PIPE)
-            host_out = host_sub.stdout.readlines()
-            for b in host_out:
-                if 'no TXT record' in b:
-                    print b.strip()
-                    record_fp.write(b)
-                    total_no_record += 1
-        print '[*] total_no_record: ', total_no_record
-
-        record_fp.close()
-
-
-
     def get_top_sites(self, url):
 
         response = requests.get(url)
@@ -62,8 +39,6 @@ class Sites(object):
 def main():
     site = Sites()
     site.pages()
-    site.get_txt_record()
-    # get_top_sites()
 
 
 if __name__ == '__main__':
